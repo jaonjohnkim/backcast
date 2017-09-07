526622c02d89b2659ba20b50e1753f4c34b4fd4f
jaonjohnkim <kim.jaon79@gmail.com>
2017-09-06 23:48:57 -0700

    attempt #3

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -3,23 +3,25 @@ var VideoListEntryView = Backbone.View.extend({
   initialize: function (entry) {
     // console.log('entry: ', entry);
     this.model = entry.model;
+    this.on('click', function() {
+      console.log('e');
+    });
     
     this.render(entry);
 
-    
   },  
 
   render: function() {
     var templated = this.template(this.model.attributes);
     // console.log(templated);
     this.$el.html(templated);
-    console.log(this.$el.find('.video-list-entry-title')[0]);
+    console.log(this.el);
     // this.model.on('click', function() {
     //   console.log('yay');
     // }, this);
-    // this.$el.find('.video-list-entry-title')[0].on('click', function() {
-      // console.log('e');
-    // }, this);
+    this.$el.find('.video-list-entry-title').on('click', function(e) {
+      console.log(e); // Need to now change Model data, which then will trigger change event, so 
+    });
     return this;
   },
 
